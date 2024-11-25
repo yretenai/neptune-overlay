23833c026c8a0cc0f2359e48b0afec97f021a309
Ada <yretenai@gmail.com>
2024-11-25 19:17:48 +0000

    treeclean: unused scripts

--- neptune-dotnet/dotnet-version.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-dotnet_version_head=$(curl -I https://aka.ms/dotnet/$2/$1-productVersion.txt 2>/dev/null)
-if [[ $dotnet_version_head == *"301 Moved Permanently"* ]]; then
-    dotnet_version=$(curl -L https://aka.ms/dotnet/$2/$1-productVersion.txt 2>/dev/null)
-    echo $dotnet_version | tr -d '\r\n'
-else
-    echo $2
-fi
